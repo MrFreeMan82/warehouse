f4202008adf4c055b68cbfbf4e3e9e64aeedcf26
MrFreeMan82 <DimaZDY82@yandex.ru>
2017-04-22 16:06:37 +0300

    Begin new page "UserListPage"

--- src/main/java/com/warehouse/client/page/MainPage.java
+++ src/main/java/com/warehouse/client/page/MainPage.java
@@ -2,7 +2,9 @@ package com.warehouse.client.page;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -17,9 +19,15 @@ public class MainPage extends Page
     interface MainUIBinder extends UiBinder<Widget, MainPage> {}
     private static final MainUIBinder binder = GWT.create(MainUIBinder.class);
 
+    @UiField DockLayoutPanel mainLayout;
+
+
     public MainPage()
     {
         initWidget(binder.createAndBindUi(this));
+
+        UserListPage userList = new UserListPage();
+        mainLayout.add(userList);
         RootLayoutPanel.get().add(this);
     }
 }
---
+++ src/main/java/com/warehouse/client/page/UserListPage.java
@@ -0,0 +1,119 @@
+package com.warehouse.client.page;
+
+import com.google.gwt.cell.client.CheckboxCell;
+import com.google.gwt.cell.client.DateCell;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.DefaultSelectionEventManager;
+import com.google.gwt.view.client.SelectionModel;
+import com.google.gwt.view.client.SingleSelectionModel;
+import org.gwtbootstrap3.client.ui.gwt.CellTable;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Дима on 22.04.2017.
+ *
+ */
+
+public class UserListPage extends Page
+{
+    @UiTemplate("com.warehouse.client.view.UserListView.ui.xml")
+    interface UserListUIBinder extends UiBinder<Widget, UserListPage> {}
+    private static final UserListUIBinder binder = GWT.create(UserListUIBinder.class);
+
+    @UiField VerticalPanel listPanel;
+
+    /**
+     * A simple data type that represents a contact.
+     */
+    private static class Contact {
+        private final String address;
+        private final Date birthday;
+        private final String name;
+
+        public Contact(String name, Date birthday, String address) {
+            this.name = name;
+            this.birthday = birthday;
+            this.address = address;
+        }
+    }
+
+    /**
+     * The list of data to display.
+     */
+    private static final List<Contact> CONTACTS = Arrays.asList(
+            new Contact("John", new Date(80, 4, 12), "123 Fourth Avenue"),
+            new Contact("Joe", new Date(85, 2, 22), "22 Lance Ln"),
+            new Contact("George",new Date(46, 6, 6),"1600 Pennsylvania Avenue"));
+
+    public UserListPage()
+    {
+        initWidget(binder.createAndBindUi(this));
+
+        CellTable<Contact> table = new CellTable<>();
+        table.setWidth("100%", true);
+        table.setAutoHeaderRefreshDisabled(true);
+        table.setAutoFooterRefreshDisabled(true);
+        table.setCondensed(true);
+        table.setStriped(true);
+
+        // Add a selection model so we can select cells.
+        final SelectionModel<Contact> selectionModel = new SingleSelectionModel<>();
+        table.setSelectionModel(selectionModel, DefaultSelectionEventManager.createCheckboxManager());
+
+        Column<Contact, Boolean> checkColumn = new Column<Contact, Boolean>(
+                new CheckboxCell(true, false)) {
+            @Override
+            public Boolean getValue(Contact object) {
+                // Get the value from the selection model.
+                return selectionModel.isSelected(object);
+            }
+        };
+        table.addColumn(checkColumn, SafeHtmlUtils.fromSafeConstant("<br/>"));
+        table.setColumnWidth(checkColumn, 40, Style.Unit.PX);
+
+        TextColumn<Contact> nameColumn =
+                new TextColumn<Contact>() {
+                    @Override
+                    public String getValue(Contact object) {
+                        return object.name;
+                    }
+                };
+        table.addColumn(nameColumn, "Name");
+
+        DateCell dateCell = new DateCell();
+        Column<Contact, Date> dateColumn
+                = new Column<Contact, Date>(dateCell) {
+            @Override
+            public Date getValue(Contact object) {
+                return object.birthday;
+            }
+        };
+        table.addColumn(dateColumn, "Birthday");
+
+        TextColumn<Contact> addressColumn
+                = new TextColumn<Contact>() {
+            @Override
+            public String getValue(Contact object) {
+                return object.address;
+            }
+        };
+        table.addColumn(addressColumn, "Address");
+
+        table.setRowCount(CONTACTS.size(), true);
+        table.setRowData(0, CONTACTS);
+
+        listPanel.add(table);
+    }
+}
